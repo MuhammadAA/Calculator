8111fa43e440e65bb26b72294d823ee21f038a6d
Muhammad Ansary <84735386+MuhammadAA@users.noreply.github.com>
2022-07-08 00:03:15 -0700

    Layout Update
    - Created Functions that perform operations
    
    - Updated user interface to create sections where buttons will fit
    
    - Added appropriate HTML elements to the project, which will later represent buttons and provide functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+function add (numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function sub (numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply (numOne, numTwo){
+    return numOne * numTwo;
+}
+
+function divide (numOne, numTwo){
+    return numOne / numTwo;
+}
+
+function negate (num){
+    return num * -1;
+}
